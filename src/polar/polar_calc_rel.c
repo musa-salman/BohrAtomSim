157db9326699d74314ea799cc9149762224b1d6b
musa-salman <musa.salman.g1@gmail.com>
2024-10-16 14:44:23 +0300

    Refactor polar_calc_rel.c use long double for more precision

--- src/polar/polar_calc_rel.c
+++ src/polar/polar_calc_rel.c
@@ -15,27 +15,29 @@ long double calc_rel_phi_dot(long double l, long double gamma, long double r, lo
 
 long double calc_rel_gamma(long double l, long double m, long double r, long double r_dot)
 {
-    double arg1 = C;
+    long double arg1 = C;
     arg1 *= m * r;
     arg1 = l / arg1;
     arg1 *= arg1;
     arg1++;
 
-    double arg2 = C;
+    long double arg2 = C;
     arg2 = r_dot / arg2;
     arg2 *= arg2;
     arg2 = 1 - arg2;
 
-    arg1 = sqrt(arg1 / arg2);
+    arg1 = sqrtl(arg1 / arg2);
 
     return arg1;
 }
 
 long double calc_rel_r_dot_dot(long double l_sqr, long double m, long double gamma, long double r, long double e, long double r_dot)
 {
-    double arg1, arg2, arg3;
+    long double arg1;
+    long double arg2;
+    long double arg3;
 
-    double r_sq = r * r;
+    long double r_sq = r * r;
 
     arg1 = l_sqr;
     arg1 /= gamma * m * r_sq * r;
