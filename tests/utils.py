db2aa40651c34d33f7667c3ecac5017e348f3bda
musa-salman <musa.salman.g1@gmail.com>
2024-10-27 20:43:59 +0200

    Add utils.py to wrap C library functions and make BohrModule a singleton class than contains simulation library reference

---
+++ tests/utils.py
@@ -0,0 +1,26 @@
+import ctypes
+
+
+def wrap_function(lib, fn_name, restype, argtypes):
+    func = lib.__getattr__(fn_name)
+    func.restype = restype
+    func.argtypes = argtypes
+    return func
+
+
+class BohrModule:
+    __mod = None
+    __module_path = "lib/libbohr_simulation.so"
+
+    def __init__(self, module_path=None):
+        if BohrModule.__mod is not None:
+            raise RuntimeError("BohrMod is a singleton class")
+        if module_path is not None:
+            BohrModule.__module_path = module_path
+        BohrModule.__mod = ctypes.CDLL(BohrModule.__module_path)
+
+    @staticmethod
+    def get_bohr_module():
+        if BohrModule.__mod is None:
+            BohrModule.__mod = ctypes.CDLL(BohrModule.__module_path)
+        return BohrModule.__mod
