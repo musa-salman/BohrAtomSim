08dc244366a8813a571df5a22f383ab3feca80a8
musa-salman <musa.salman.g1@gmail.com>
2024-12-22 09:10:00 +0200

    Add AddSimulationDialog class for creating and managing simulation input dialog

---
+++ include/view/AddSimulationDialog.hpp
@@ -0,0 +1,24 @@
+#ifndef ADD_SIMULATION_DIALOG_HPP
+#define ADD_SIMULATION_DIALOG_HPP
+
+#include <functional>
+
+#include "Component.hpp"
+#include "Simulation.hpp"
+#include "atom/atom_bohr_sim.h"
+#include "hdf5/SimulationFields.hpp"
+
+class AddSimulationDialog : public Component {
+  public:
+    explicit AddSimulationDialog(
+        const std::function<void(Simulation)> &on_submit);
+
+    void draw() override;
+
+  private:
+    const std::function<void(Simulation)> on_submit;
+
+    Simulation simulation = Simulation("", POLAR, SIMULATION_2D_NON_REL);
+};
+
+#endif
---
+++ src/view/AddSimulationDialog.cpp
@@ -0,0 +1,61 @@
+#include <imgui.h>
+
+#include "view/AddSimulationDialog.hpp"
+
+AddSimulationDialog::AddSimulationDialog(
+    const std::function<void(Simulation)> &on_submit)
+    : on_submit(on_submit) {}
+
+void AddSimulationDialog::draw() {
+    if (ImGui::Button("Add Simulation")) {
+        ImGui::OpenPopup("Add Simulation");
+    }
+
+    if (ImGui::Begin("Add Simulation")) {
+
+        // Simulation name
+        ImGui::InputText("Name", simulation.name, sizeof(simulation.name));
+        ImGui::Separator();
+
+        // Simulation type
+        ImGui::Text("Simulation Type");
+        ImGui::RadioButton("2D non-relativistic", &simulation.type, POLAR);
+        ImGui::RadioButton("3D non-relativistic", &simulation.type, SPHERICAL);
+        ImGui::RadioButton("2D relativistic", &simulation.type, REL_POLAR);
+        ImGui::RadioButton("3D relativistic", &simulation.type, REL_SPHERICAL);
+        ImGui::Separator();
+
+        // Electron orbit
+        ImGui::Text("Electron Orbit");
+        ImGui::InputScalar("Principal", ImGuiDataType_U8,
+                           &simulation.orbit.principal);
+        ImGui::InputScalar("Angular", ImGuiDataType_U8,
+                           &simulation.orbit.angular);
+        ImGui::InputScalar("Magnetic", ImGuiDataType_U8,
+                           &simulation.orbit.magnetic);
+        ImGui::Separator();
+
+        // Simulation parameters
+        ImGui::Text("Simulation Parameters");
+        ImGui::InputFloat("Revolutions", &simulation.revolutions);
+        ImGui::InputScalar("Time Interval", ImGuiDataType_Double,
+                           &simulation.time_interval);
+        ImGui::Separator();
+
+        // Submit button
+        if (ImGui::Button("Submit")) {
+            on_submit(simulation);
+
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::SameLine();
+
+        // Cancel button
+        if (ImGui::Button("Cancel")) {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::End();
+    }
+}
