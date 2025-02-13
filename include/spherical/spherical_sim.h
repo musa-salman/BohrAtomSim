4353f7595d31965a4b4625e7c80289dbd5ab64f3
musa-salman <musa.salman.g1@gmail.com>
2025-02-13 09:22:54 +0200

    Add header file for spherical simulation functionality

---
+++ include/spherical/spherical_sim.h
@@ -0,0 +1,16 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef SPHERICAL_SIM_HPP
+#define SPHERICAL_SIM_HPP
+
+#include "atom/atom_bohr_sim.h"
+
+void simulate_spherical_orbit(const struct sim_ctx ctx);
+
+#endif // SPHERICAL_SIM_HPP
+
+#ifdef __cplusplus
+}
+#endif
