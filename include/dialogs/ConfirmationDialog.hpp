6d7f5697049b7cad3efe7ff3349502f1e25156c2
musa-salman <musa.salman.g1@gmail.com>
2025-02-13 09:21:45 +0200

    Add ConfirmationDialog class for user confirmation prompts

---
+++ include/dialogs/ConfirmationDialog.hpp
@@ -0,0 +1,22 @@
+#ifndef CONFIRMATIONDIALOG_HPP
+#define CONFIRMATIONDIALOG_HPP
+
+#include <functional>
+#include <string>
+
+#include "view/Component.hpp"
+
+class ConfirmationDialog : public Component {
+  public:
+    explicit ConfirmationDialog(const std::string &message);
+
+    void open(const std::function<void()> &onConfirm);
+    void draw() override;
+
+  private:
+    std::string message;
+    bool isOpen = false;
+    std::function<void()> confirmCallback;
+};
+
+#endif // CONFIRMATIONDIALOG_H
---
+++ src/dialogs/ConfirmationDialog.cpp
@@ -0,0 +1,39 @@
+#include <imgui.h>
+
+#include "dialogs/ConfirmationDialog.hpp"
+
+ConfirmationDialog::ConfirmationDialog(const std::string &message)
+    : message(message), isOpen(false), confirmCallback(nullptr) {}
+
+void ConfirmationDialog::open(const std::function<void()> &onConfirm) {
+    confirmCallback = onConfirm;
+    isOpen = true;
+}
+
+void ConfirmationDialog::draw() {
+    if (isOpen) {
+        ImGui::OpenPopup("Confirmation");
+    }
+
+    if (ImGui::BeginPopupModal("Confirmation", nullptr,
+                               ImGuiWindowFlags_AlwaysAutoResize)) {
+        ImGui::Text("%s", message.c_str());
+        ImGui::Separator();
+
+        if (ImGui::Button("Yes")) {
+            if (confirmCallback)
+                confirmCallback();
+            ImGui::CloseCurrentPopup();
+            isOpen = false;
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("No")) {
+            ImGui::CloseCurrentPopup();
+            isOpen = false;
+        }
+
+        ImGui::EndPopup();
+    }
+}
