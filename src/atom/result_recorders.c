7f91bb3c4f458a6e16444cbcc30b89ab0eeaa403
musa-salman <musa.salman.g1@gmail.com>
2025-02-13 09:39:19 +0200

    Refactor result_recorders.c to replace CSV recording with binary file recording for improved performance and data handling

--- src/atom/result_recorders.c
+++ src/atom/result_recorders.c
@@ -4,24 +4,22 @@
 #include "utils/iterator.h"
 #include "utils/macros.h"
 
-void record2csv(void *record_in, const struct sim_itr *iter) {
-    FILE *csv = (FILE *)record_in;
+void record2bin(FILE *bin_file, const struct sim_itr *iter) {
+    // Prepare data buffer
+    double data[] = {iter->dt,        RHO(iter), R_DOT(iter),
+                     R_DOT_DOT(iter), PHI(iter), PHI_DOT(iter)};
 
-    fprintf(csv, "%.20E,%.20E,%.20E,%.20E,%.20E,%.20E", iter->dt,
-            R(iter), R_DOT(iter), R_DOT_DOT(iter), PHI(iter), PHI_DOT(iter));
+    // Write the fixed data
+    fwrite(data, sizeof(double), 6, bin_file);
 
     if (THETA(iter) != -1) {
-        fprintf(csv, ",%.20E,%.20E,%.20E", THETA(iter), THETA_DOT(iter),
-                THETA_DOT_DOT(iter));
-    }
-
-    if (EPSILON(iter) != -1) {
-        fprintf(csv, ",%.20E", EPSILON(iter));
+        double theta_data[] = {THETA(iter), THETA_DOT(iter),
+                               THETA_DOT_DOT(iter)};
+        fwrite(theta_data, sizeof(double), 3, bin_file);
     }
 
     if (GAMMA(iter) != -1) {
-        fprintf(csv, ",%.20E,%.20E", GAMMA(iter), DELTA_PHI(iter));
+        double gamma_data[] = {GAMMA(iter), DELTA_PHI(iter)};
+        fwrite(gamma_data, sizeof(double), 2, bin_file);
     }
-
-    fprintf(csv, "\n");
 }
