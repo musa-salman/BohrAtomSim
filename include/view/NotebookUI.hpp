4cd2c3e55204dcaed420439c4712ac0efb4b9e76
musa-salman <musa.salman.g1@gmail.com>
2025-02-13 09:27:01 +0200

    Add NotebookUI class for managing and displaying notebooks in the user interface

---
+++ include/view/NotebookUI.hpp
@@ -0,0 +1,37 @@
+#ifndef NOTEBOOK_UI_HPP
+#define NOTEBOOK_UI_HPP
+
+#include <vector>
+
+#include "dialogs/ConfirmationDialog.hpp"
+#include "plotter/SimulationVisualizer.hpp"
+#include "simulation_repositories/ArchivedSimulationManager.hpp"
+#include "simulation_repositories/NotebookRepository.hpp"
+#include "view/Component.hpp"
+#include "view/PlotUI.hpp"
+
+class NotebookUI : public Component {
+  private:
+    NotebookRepository &notebookRepository;
+    ArchivedSimulationManager &archivedManager;
+    SimulationRepository &simulationRepository;
+    SimulationVisualizer visualizer;
+
+    PlotUI plotUI;
+    std::vector<Notebook> notebooks;
+    char notebookName[128];        // NOSONAR
+    char notebookDescription[256]; // NOSONAR
+    size_t selectedNotebookID;
+    bool showNotebookPopup;
+    ConfirmationDialog deleteNotebookDialog;
+
+  public:
+    explicit NotebookUI(NotebookRepository &repo,
+                        SimulationRepository &simulationRepository,
+                        ArchivedSimulationManager &archivedManager);
+    void draw() override;
+    void loadNotebooks();
+    size_t getSelectedNotebookID();
+};
+
+#endif // NOTEBOOK_UI_HPP
