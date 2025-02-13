1c8d2419a3e04c0341d6a4b923b6b07668f19bf8
musa-salman <musa.salman.g1@gmail.com>
2025-02-13 09:38:25 +0200

    Refactor SimulationExplorer to integrate OngoingSimulationManager and Simulator, enhancing simulation management and plotting capabilities

--- include/view/SimulationExplorer.hpp
+++ include/view/SimulationExplorer.hpp
@@ -1,30 +1,30 @@
 #ifndef SIMULATION_MANAGER_VIEW_HPP
 #define SIMULATION_MANAGER_VIEW_HPP
 
-#include <functional>
 #include <imgui.h>
-#include <memory>
-#include <string>
-#include <unordered_map>
 
 #include "Component.hpp"
-#include "Simulation.hpp"
-#include "hdf5/SimulationManager.hpp"
+#include "explorer_manager/OngoingSimulationManager.hpp"
+#include "simulator_runner/Simulation.hpp"
+#include "simulator_runner/Simulator.hpp"
+#include "view/AddSimulationDialog.hpp"
+#include "view/SimulationCard.hpp"
 
 class SimulationExplorer : public Component {
   public:
-    explicit SimulationExplorer(
-        const SimulationManager &simulation_manager,
-        const std::function<void(Simulation &)> &on_simulation_selected);
+    explicit SimulationExplorer(OngoingSimulationManager &simulation_manager,
+                                Simulator &simulator);
 
     void draw() override;
 
   private:
-    const SimulationManager &simulation_manager;
-    std::function<void(Simulation &)> on_simulation_selected;
-    const std::unordered_map<std::string, std::shared_ptr<Simulation>>
-        &simulations;
-    std::string selected_simulation;
+    AddSimulationDialog add_simulation_interface;
+    SimulationCard simulation_card;
+    OngoingSimulationManager &simulation_manager;
+    Simulator &simulator;
+    const std::unordered_map<size_t, std::shared_ptr<Simulation>> &simulations;
+    std::unordered_map<std::string, bool> plot_selection;
+    size_t selected_simulation;
 };
 
 #endif // SIMULATION_MANAGER_H
--- src/view/SimulationExplorer.cpp
+++ src/view/SimulationExplorer.cpp
@@ -1,35 +1,135 @@
 #include <IconsFontAwesome4.h>
 #include <imgui.h>
+#include <implot.h>
+#include <sys/stat.h>
 
+#include "view/AddSimulationDialog.hpp"
 #include "view/SimulationExplorer.hpp"
 
 SimulationExplorer::SimulationExplorer(
-    const SimulationManager &simulation_manager,
-    const std::function<void(Simulation &)> &on_simulation_selected)
-    : simulation_manager(simulation_manager),
-      on_simulation_selected(on_simulation_selected),
+    OngoingSimulationManager &_simulation_manager, Simulator &_simulator)
+    : simulation_manager(_simulation_manager), simulator(_simulator),
       simulations(simulation_manager.getSimulations()) {
+    add_simulation_interface.setOnSubmit([this](const Simulation &simulation) {
+        simulation_manager.addSimulation(simulation);
+    });
+
+    simulation_card.setOnSimulate([this](const Simulation *simulation) {
+        sim_ctx ctx;
+
+        ctx.id = simulation->id;
+
+        ctx.record_interval = simulation->record_interval;
+        ctx.time_interval = simulation->time_interval;
+        ctx.revolutions = simulation->revolutions;
+
+        ctx.orbit = simulation->orbit;
+
+        simulator.simulateOrbit(ctx, simulation->type, [this, simulation]() {
+            simulation_manager.markSimulationAsComplete(simulation->id);
+            auto monitor = simulation_manager.getMonitor(simulation->id);
+            if (monitor.has_value()) {
+                monitor->get()->stopMonitoring();
+            }
+        });
+
+        auto monitor = simulation_manager.getMonitor(simulation->id);
+        if (monitor.has_value()) {
+            monitor->get()->startMonitoring();
+        }
+    });
+
+    this->selected_simulation = std::numeric_limits<size_t>::max();
+
     if (!simulations.empty()) {
         selected_simulation = simulations.begin()->first;
     }
 }
 
 void SimulationExplorer::draw() {
-    ImGui::Begin("Simulations Explorer");
+    ImGui::Begin("Simulation Runner", nullptr,
+                 ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_MenuBar);
 
-    ImGui::Separator();
+    ImGuiID dockspace_id = ImGui::GetID("SimExplorerDockSpace");
+    ImGui::DockSpace(dockspace_id, ImVec2(0, 0), ImGuiDockNodeFlags_None);
 
-    for (auto &[name, simulation] : simulations) {
-        ImGui::PushID(name.c_str());
+    ImGui::End();
 
-        // Make simulation name selectable
-        if (ImGui::Selectable(name.c_str(), selected_simulation == name)) {
-            selected_simulation = name;
-            on_simulation_selected(*simulation);
+    ImGui::Begin("Simulation Manager", nullptr, ImGuiWindowFlags_NoCollapse);
+    {
+        ImGui::Text("Simulations");
+        add_simulation_interface.draw();
+
+        for (auto const &[id, simulation] : simulations) {
+            if (ImGui::Selectable(simulation->name.c_str(),
+                                  selected_simulation == id)) {
+                selected_simulation = id;
+                simulation_card.setSimulation(*simulation);
+            }
         }
-
-        ImGui::PopID();
     }
+    ImGui::End();
 
+    ImGui::Begin("Simulation Plot", nullptr, ImGuiWindowFlags_NoCollapse);
+    {
+        if (selected_simulation != std::numeric_limits<size_t>::max()) {
+            auto opt_monitor =
+                simulation_manager.getMonitor(selected_simulation);
+            std::shared_ptr<SimulationResultMonitor> monitor;
+            if (opt_monitor.has_value()) {
+                monitor = opt_monitor.value();
+            }
+            auto datasets = monitor->getDatasets();
+            simulation_card.draw();
+
+            if (!datasets.contains("t") || datasets["t"].empty()) {
+                ImGui::Text("No data available for plotting.");
+                ImGui::End();
+                return;
+            }
+
+            const auto &t_data = datasets["t"];
+            double t_min = t_data.front();
+            double t_max = t_data.back();
+
+            ImGui::Text("Select Datasets to Plot:");
+
+            ImGui::PushID("plot_selection");
+
+            for (const auto &[name, data] : datasets) {
+                if (name == "t")
+                    continue; // Skip "t"
+
+                if (!plot_selection.contains(name)) {
+                    plot_selection[name] = false;
+                }
+
+                ImGui::SameLine();
+
+                ImGui::Checkbox(name.c_str(), &plot_selection[name]);
+            }
+            ImGui::PopID();
+
+            // Loop again to plot only selected datasets
+            for (const auto &[name, data] : datasets) {
+                if (name == "t" || !plot_selection[name])
+                    continue;
+
+                if (ImPlot::BeginPlot(name.c_str())) {
+                    ImPlot::SetupAxes(nullptr, nullptr, ImPlotAxisFlags_AutoFit,
+                                      ImPlotAxisFlags_AutoFit);
+                    ImPlot::SetupAxisLimits(ImAxis_X1, t_min, t_max,
+                                            ImGuiCond_Always);
+
+                    ImPlot::PlotLine(name.c_str(), t_data.data(), data.data(),
+                                     static_cast<int>(data.size()));
+                    ImPlot::EndPlot();
+                }
+            }
+
+        } else {
+            ImGui::Text("Select a simulation to view live plotting.");
+        }
+    }
     ImGui::End();
 }
