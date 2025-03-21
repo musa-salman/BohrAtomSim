ca16382e63db9edca6de669c1bd0b8831ab09030
musa-salman <musa.salman.g1@gmail.com>
2025-03-21 21:54:32 +0200

    Add Sidebar component with collapsible view and section management

---
+++ include/view/SideBar.hpp
@@ -0,0 +1,29 @@
+#ifndef SIDEBAR_HPP
+#define SIDEBAR_HPP
+
+#include "view/Component.hpp"
+
+class Sidebar : public Component {
+  public:
+    enum Section {
+        CURRENT_SIMULATIONS,
+        ADD_SIMULATION,
+        SIMULATION_MANAGER,
+        SETTINGS,
+        NONE
+    };
+
+  private:
+    bool isCollapsed = false;
+    Section activeSection = CURRENT_SIMULATIONS;
+
+  public:
+    void render() override;
+    Section getActiveSection();
+
+  private:
+    void renderCollapsedView();
+    void renderExpandedView();
+};
+
+#endif // SIDEBAR_HPP
