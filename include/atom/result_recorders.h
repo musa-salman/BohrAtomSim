3d58c33b101f579bce97c0ce6ef809a28ac17266
musa-salman <musa.salman.g1@gmail.com>
2025-03-25 09:38:48 +0200

    Add file header structure and initialization for binary output

--- include/atom/result_recorders.h
+++ include/atom/result_recorders.h
@@ -9,6 +9,49 @@ extern "C" {
 
 #include "utils/iterator.h"
 
+#define MAX_FIELD_NAME 16
+
+struct file_header {
+    uint8_t magic[5];
+    uint8_t version;
+    uint8_t field_count;
+    uint8_t endianness;
+    uint8_t scalar_type;
+} __attribute__((packed));
+
+static inline uint8_t detect_endianness() {
+    uint16_t x = 0x0123;
+    return *((uint8_t *)&x) == 0x23 ? 0 : 1; // 0 = little, 1 = big
+}
+
+static inline uint8_t detect_scalar_type() {
+#ifdef FLOAT_SCALAR
+    return 1;
+#elif defined(LONG_DOUBLE_SCALAR)
+    return 3;
+#else
+    return 2; // default: double
+#endif
+}
+
+static inline void
+init_file_header(FILE *bin_file, const uint8_t (*field_names)[MAX_FIELD_NAME],
+                 size_t field_count) {
+    struct file_header header = {
+        .magic = {'A', 'T', 'O', 'M', 0x89},
+        .version = 1,
+        .field_count = 6,
+        .endianness = detect_endianness(),
+        .scalar_type = detect_scalar_type(),
+    };
+
+    fwrite(&header, sizeof(struct file_header), 1, bin_file);
+
+    for (size_t i = 0; i < field_count; i++) {
+        fwrite(field_names[i], sizeof(uint8_t), MAX_FIELD_NAME, bin_file);
+    }
+}
+
 void record2bin(FILE *bin_file, const struct sim_itr *iter);
 
 #endif // RESULT_RECORDER_H
