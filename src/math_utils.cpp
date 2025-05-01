415ccb7fef620adcbb5cd1f9fcd1e642f4d97abc
musa-salman <musa.salman.g1@gmail.com>
2025-05-01 13:17:24 +0300

    Add computeAngleVec2D function for 2D vector angle calculation

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -26,3 +26,21 @@ polar2cartesian(
 
     return cartesian_data;
 }
+
+double computeAngleVec2D(double u[2], double v[2]) {
+    double dot_product = u[0] * v[0] + u[1] * v[1];
+    double magnitude_u = std::sqrt(u[0] * u[0] + u[1] * u[1]);
+    double magnitude_v = std::sqrt(v[0] * v[0] + v[1] * v[1]);
+
+    if (magnitude_u < 1e-6 || magnitude_v < 1e-6) {
+        return 0.0;
+    }
+
+    double cos_theta = dot_product / (magnitude_u * magnitude_v);
+    if (cos_theta < -1.0)
+        cos_theta = -1.0;
+    if (cos_theta > 1.0)
+        cos_theta = 1.0;
+
+    return std::acos(cos_theta);
+}
