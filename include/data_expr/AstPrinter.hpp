aaf57e27f9ad65d40a6e6c56aa3a3333f313f972
musa-salman <musa.salman.g1@gmail.com>
2025-05-11 12:29:11 +0300

    Refactor AstPrinter to improve expression printing and streamline visitor methods

--- include/data_expr/AstPrinter.hpp
+++ include/data_expr/AstPrinter.hpp
@@ -1,60 +1,65 @@
+#include <string>
 
 #include "Expr.hpp"
 
-class AstPrinter : public Expr::Visitor<std::string> {
+class AstPrinter : public Expr::Visitor {
+    std::string result;
+
   public:
     std::string print(const std::shared_ptr<Expr> &expr) {
-        return expr->accept(*this);
-    }
-
-    std::string visitBinaryExpr(const Binary &expr) override {
-        return parenthesize(expr.op.lexeme, expr.left, expr.right);
-    }
-
-    std::string visitUnaryExpr(const Unary &expr) override {
-        return parenthesize(expr.op.lexeme, expr.right);
-    }
-
-    std::string visitGroupingExpr(const Grouping &expr) override {
-        return parenthesize("group", expr.expression);
-    }
-
-    std::string visitNumberExpr(const Number &expr) override {
-        return std::to_string(std::any_cast<double>(expr.value.literal));
-    }
-
-    std::string visitVariableExpr(const Variable &expr) override {
-        return std::string(expr.name.lexeme);
-    }
-
-    std::string visitCallExpr(const Call &expr) override {
-        std::string result = parenthesize("call", expr.callee);
-        for (const auto &arg : expr.args) {
-            result += " " + arg->accept(*this);
-        }
+        expr->accept(*this);
         return result;
+    }
+
+    void visitTrinaryExpr(const Trinary &expr) override {
+        parenthesize(expr.op1.lexeme, std::vector<std::shared_ptr<Expr>>{
+                                          expr.left, expr.middle, expr.right});
+    }
+
+    void visitBinaryExpr(const Binary &expr) override {
+        parenthesize(expr.op.lexeme, expr.left, expr.right);
+    }
+
+    void visitUnaryExpr(const Unary &expr) override {
+        parenthesize(expr.op.lexeme, expr.right);
+    }
+
+    void visitGroupingExpr(const Grouping &expr) override {
+        parenthesize("group", expr.expression);
+    }
+
+    void visitNumberExpr(const Number &expr) override {
+        result += std::to_string(std::any_cast<double>(expr.value.literal));
+    }
+
+    void visitVariableExpr(const Variable &expr) override {
+        result += expr.name.lexeme;
+    }
+
+    void visitCallExpr(const Call &expr) override {
+        parenthesize("call " + expr.callee.lexeme, expr.args);
     }
 
   private:
-    std::string parenthesize(const std::string &name,
-                             const std::shared_ptr<Expr> &expr) {
-        return parenthesize(name, std::vector<std::shared_ptr<Expr>>{expr});
+    void parenthesize(const std::string &name,
+                      const std::shared_ptr<Expr> &expr) {
+        parenthesize(name, std::vector<std::shared_ptr<Expr>>{expr});
     }
 
-    std::string parenthesize(const std::string &name,
-                             const std::vector<std::shared_ptr<Expr>> &exprs) {
-        std::string result = "(" + name;
-        for (const auto &expr : exprs)
-            result += " " + expr->accept(*this);
+    void parenthesize(const std::string &name,
+                      const std::vector<std::shared_ptr<Expr>> &exprs) {
+        result += "(" + name;
+        for (const auto &expr : exprs) {
+            result += " ";
+            expr->accept(*this);
+        }
 
         result += ")";
-        return result;
     }
 
-    std::string parenthesize(const std::string &name,
-                             const std::shared_ptr<Expr> &left,
-                             const std::shared_ptr<Expr> &right) {
-        return parenthesize(name,
-                            std::vector<std::shared_ptr<Expr>>{left, right});
+    void parenthesize(const std::string &name,
+                      const std::shared_ptr<Expr> &left,
+                      const std::shared_ptr<Expr> &right) {
+        parenthesize(name, std::vector<std::shared_ptr<Expr>>{left, right});
     }
 };
